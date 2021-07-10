404e779d5682d891b207e29a8edbb45f5416f0e7
saivijayakumar <bollusaivijaykumar212@gmail.com>
2021-07-10 21:12:40 +0530

    [SaiVijayaKumar] Commiting Welcome Message

---
+++ LinkedList/LinkedListMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class LinkedListMethods
+    {
+        //Creating Both Head and Tail reference
+        public Node head;
+        public Node tail;
+        public void InsertAtLast(int data)
+        {
+            Node newnode = new Node(data);
+            if(head==null)
+            {
+                this.head = this.tail = newnode;
+                
+            }
+            else
+            {
+                this.tail.next = newnode;
+                this.tail = newnode;
+            }
+        }
+        public void DisplayList()
+        {
+            int count=0;
+            Node temp = this.head;
+            while(temp!=null)
+            {
+                Console.WriteLine("At {0} Index We Have {1} value",count,temp.data);
+                count++;
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        /// <summary>
+        /// ---------->Welcome To LinkedList<--------------
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---------->Welcome To LinkedList<--------------\n");
+            LinkedListMethods linked = new LinkedListMethods();
+            linked.InsertAtLast(56);
+            linked.InsertAtLast(30);
+            linked.InsertAtLast(70);
+            linked.DisplayList();
+            Console.ReadLine();
+        }
+    }
+}
